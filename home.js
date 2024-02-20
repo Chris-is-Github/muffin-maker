b231a0781675f86856f5742f99df394c8208590b
john <forderman.ben@gmail.com>
2024-02-20 13:11:20 +0100

    home changed

---
+++ home.js
@@ -0,0 +1,21 @@
+function printMessage(){
+   const muffin = document.querySelector('.muffin');
+   muffin.style.backgroundColor = '#ffac09';
+   muffin.style.boxShadow = '0 0 40px #ffac09';
+   muffin.style.transition = '2.5s ease';
+
+}
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+
+function toppingright(){
+
+   images[0] = bilder/PENUP_20240123_184918.jpg; 
+   iamges[1] = bilder/PENUP_20240123_184931.jpg;
+
+   document.slide.src = images[0];
+}
+
+   
