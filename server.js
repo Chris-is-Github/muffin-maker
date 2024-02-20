8ec4b4ec26564dfb1ded337115ee9dfc5a66ed8d
Chris-is-Github <chdkraus@gmail.com>
2024-02-20 15:12:43 +0100

    Einlogg Änderung

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const users = {};
 
 app.post('/register', (req, res) => {
     const username = req.body.username.toLowerCase();
-    if(req.body.password != req.body.password2){
-        return res.send("Passwörter stimmen nicht überein");
-    }
     const { password } = req.body;
     if (users[username]) {
         return res.send('Benutzername bereits vergeben.');
+    }
+    if(req.body.password != req.body.password2){
+        return res.send("Passwörter stimmen nicht überein");
     }
     users[username] = { password };
     fs.writeFileSync('./users.json', JSON.stringify(users, null, 2));
