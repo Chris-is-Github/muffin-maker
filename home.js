70671dbfae46d9fdbe8107347e5be1ed24d712b5
john <forderman.ben@gmail.com>
2024-03-01 13:16:48 +0100

    all home

--- home.js
+++ home.js
@@ -3,67 +3,63 @@ let topping = [];
 let muffin = [];
 let icing = [];
 
-let icing_id  ;
-let topping_id ;
-let muffinBase_id ;
+let icing_id;
+let topping_id;
+let muffinBase_id;
 
-fetch('/muffins')
-  .then(response => {
+fetch("/muffins")
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Netzwerkantwort nicht ok');
+      throw new Error("Netzwerkantwort nicht ok");
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     muffin = data.muffins;
     document.getElementById("muffin-home").src = muffin[0].imageUrl;
     document.getElementById("text-muffin").textContent = muffin[0].name;
     console.log(muffin);
   })
-  .catch(error => {
-    console.error('Fehler beim Fetchen der Muffin-Daten:', error);
+  .catch((error) => {
+    console.error("Fehler beim Fetchen der Muffin-Daten:", error);
   });
 
-fetch('/toppings')
-  .then(response => {
+fetch("/toppings")
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Netzwerkantwort nicht ok');
+      throw new Error("Netzwerkantwort nicht ok");
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     topping = data.toppings;
     document.getElementById("topping-home").src = topping[0].imageUrl;
     document.getElementById("text-topping").textContent = topping[0].name;
     console.log(topping);
   })
-  .catch(error => {
-    console.error('Fehler beim Fetchen der Muffin-Daten:', error);
+  .catch((error) => {
+    console.error("Fehler beim Fetchen der Muffin-Daten:", error);
   });
 
-fetch('/icings')
-  .then(response => {
+fetch("/icings")
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Netzwerkantwort nicht ok');
+      throw new Error("Netzwerkantwort nicht ok");
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     icing = data.icings;
     console.log(icing);
     document.getElementById("icing-home").src = icing[0].imageUrl;
     document.getElementById("text-icing").textContent = icing[0].name;
   })
-  .catch(error => {
-    console.error('Fehler beim Fetchen der Muffin-Daten:', error);
+  .catch((error) => {
+    console.error("Fehler beim Fetchen der Muffin-Daten:", error);
   });
 
-
-
 function change_muffin(button) {
-
   switch (button) {
-
     case 1:
       i--;
       if (i < 0) {
@@ -71,7 +67,7 @@ function change_muffin(button) {
       }
       document.getElementById("icing-home").src = icing[i].imageUrl;
       document.getElementById("text-icing").textContent = icing[i].name;
-     break;
+      break;
 
     case 2:
       i++;
@@ -81,8 +77,6 @@ function change_muffin(button) {
       document.getElementById("icing-home").src = icing[i].imageUrl;
       document.getElementById("text-icing").textContent = icing[i].name;
       break;
-
-
 
     case 3:
       i--;
@@ -119,53 +113,37 @@ function change_muffin(button) {
       document.getElementById("text-muffin").textContent = muffin[i].name;
       break;
 
-
     default:
       break;
   }
-
-
-
 }
 
-function safe_muffin(){
-
-
-
-}
-
-
-
-
-
+function safe_muffin() {}
 
 // muffin Speichern (noch nicht getestet)
 
-function safe_muffin(){
-
-  
-
-  
-  let icing_id = icing[i].id; 
-  let topping_id = topping[i].id; 
-  let muffinBase_id = muffin[i].id; 
+function safe_muffin() {
+  let icing_id = icing[i].id;
+  let topping_id = topping[i].id;
+  let muffinBase_id = muffin[i].id;
 
   /* console.log(icing);  */
-  
-  console.log(topping_id); 
-  console.log(muffinBase_id); 
-  console.log(icing_id); 
-  
+
+  console.log(topping_id);
+  console.log(muffinBase_id);
+  console.log(icing_id);
+
   const requestOptions = {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ icing_id, topping_id, muffinBase_id }),
-  credentials: 'include' 
-};
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ icing_id, topping_id, muffinBase_id }),
+    credentials: "include",
+  };
 
-fetch('/addMuffin', requestOptions)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Fehler beim Hinzufügen des Muffins:', error)); 
-
+  fetch("/addMuffin", requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) =>
+      console.error("Fehler beim Hinzufügen des Muffins:", error)
+    );
 }
